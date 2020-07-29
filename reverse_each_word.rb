adfbcf9e6da0aa1087873bf30072fbc9b429201b
Kevin Britt <kbritt510@gmail.com>
2020-07-29 23:06:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_array = []
   sentence.split.collect do |word|
-    new_array.push("word.reverse")
+    new_array.push(word.reverse)
     new_array.join(" ")
   end  
   return new_array
